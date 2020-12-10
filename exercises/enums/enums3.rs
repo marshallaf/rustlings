dd7a4780749ed447bf5a7f6be828b311761e2274
Andrew Marshall <marshall.a.f@gmail.com>
2020-12-09 20:21:32 -0600

    enums3

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -4,7 +4,10 @@
 // I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit
 }
 
 struct Point {
